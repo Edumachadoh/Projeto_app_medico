1fa268d0b27aae90f3d9919820c8742125883371
Ana Paula Endler <anapaulaendler@gmail.com>
2025-08-22 10:11:21 -0300

    fix: Doctor and Patient Repository Interface

--- app/src/main/java/com/up/clinica_digital/domain/repository/DoctorRepository.kt
+++ app/src/main/java/com/up/clinica_digital/domain/repository/DoctorRepository.kt
@@ -2,6 +2,6 @@ package com.up.clinica_digital.domain.repository
 
 import com.up.clinica_digital.domain.model.Doctor
 
-interface DoctorRepository : UserRepository<Doctor> {
+interface DoctorRepository : CrudRepository<Doctor> {
     suspend fun validateCRM(crm: String): Boolean
 }
--- app/src/main/java/com/up/clinica_digital/domain/repository/PatientRepository.kt
+++ app/src/main/java/com/up/clinica_digital/domain/repository/PatientRepository.kt
@@ -2,4 +2,4 @@ package com.up.clinica_digital.domain.repository
 
 import com.up.clinica_digital.domain.model.Patient
 
-interface PatientRepository : UserRepository<Patient>
+interface PatientRepository : CrudRepository<Patient>
