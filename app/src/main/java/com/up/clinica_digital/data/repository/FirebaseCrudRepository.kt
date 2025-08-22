e8f7ba8b3fd2caaf27b6e767457fe70d572312c1
Ana Paula Endler <anapaulaendler@gmail.com>
2025-08-22 08:52:08 -0300

    feat: firebase generic repository

---
+++ app/src/main/java/com/up/clinica_digital/data/repository/FirebaseCrudRepository.kt
@@ -0,0 +1,41 @@
+package com.up.clinica_digital.data.repository
+
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.up.clinica_digital.domain.common.HasId
+import com.up.clinica_digital.domain.repository.CrudRepository
+import kotlinx.coroutines.tasks.await
+
+abstract class FirebaseCrudRepository<T : HasId>(
+    private val collectionPath: String,
+    private val firestore: FirebaseFirestore
+) : CrudRepository<T> {
+
+    protected val collection = firestore.collection(collectionPath)
+
+    abstract fun T.toMap(): Map<String, Any>
+    abstract fun DocumentSnapshot.toDomain(): T?
+
+    override suspend fun create(item: T): Boolean = try {
+        collection.document(item.id).set(item.toMap()).await()
+        true
+    } catch (e: Exception) { false }
+
+    override suspend fun list(): List<T> = try {
+        collection.get().await().documents.mapNotNull { it.toDomain() }
+    } catch (e: Exception) { emptyList() }
+
+    override suspend fun getById(id: String): T? = try {
+        collection.document(id).get().await().toDomain()
+    } catch (e: Exception) { null }
+
+    override suspend fun update(item: T): Boolean = try {
+        collection.document(item.id).update(item.toMap()).await()
+        true
+    } catch (e: Exception) { false }
+
+    override suspend fun delete(id: String): Boolean = try {
+        collection.document(id).delete().await()
+        true
+    } catch (e: Exception) { false }
+}
