6f28846025b21873939329dcf80418adac8ccc0c
Ana Paula Endler <anapaulaendler@gmail.com>
2025-10-16 19:16:42 -0300

    chore: update color palette

--- app/src/main/java/com/up/clinica_digital/ui/theme/Color.kt
+++ app/src/main/java/com/up/clinica_digital/ui/theme/Color.kt
@@ -3,13 +3,14 @@ package com.up.clinica_digital.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Paleta
-val PrimaryLight = Color(0xFF034AA6)
-val PrimaryDark = Color(0xFF5AA0FC)
+val PrimaryLight = Color(0xFFB93B4D)
+val PrimaryDark = Color(0xFF9C2440)
 
-val SecondaryLight = Color(0xFFA2C5F2)
-val SecondaryDark = Color(0xFF023373)
+val SecondaryLight = Color(0xFFF2B8C6)
+val SecondaryDark = Color(0xFF7A1029)
 
-val Accent = Color(0xFF2C7BBF)
+val Accent = Color(0xFFE85D75)
 
-val BackgroundLight = Color(0xFFF2F2F0)
-val BackgroundDark = Color(0xFF0B0B0B)
+// Backgrounds
+val BackgroundLight = Color(0xFFFDF2F2)
+val BackgroundDark = Color(0xFF1C0C0F)
